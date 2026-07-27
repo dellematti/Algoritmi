d9ab40cb8c428a611a4fdf11848a35abf3364809
Mattia Delledonne <116433753+dellematti@users.noreply.github.com>
2026-07-27 23:25:57 +0200

    Update

--- mergeSort.ml
+++ mergeSort.ml
@@ -13,7 +13,6 @@ let noble_gases : element list =
   ; ("neon", 10)
   ]
 
-
 let print_elements (lst : element list) : unit =
   let print_one (name, atomic_number) =
     Printf.printf "%s, %d; " name atomic_number
