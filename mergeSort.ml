237ac48a513f808a7d47fcc7abb46e5d460ea92f
Mattia Delledonne <116433753+dellematti@users.noreply.github.com>
2026-09-02 23:24:59 +0200

    update
    Remove unnecessary blank line before print_elements function.

--- mergeSort.ml
+++ mergeSort.ml
@@ -14,7 +14,6 @@ let noble_gases : element list =
   ]
 
 
-
 let print_elements (lst : element list) : unit =
   let print_one (name, atomic_number) =
     Printf.printf "%s, %d; " name atomic_number
